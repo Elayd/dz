e034380286664daab0d1b0aa291a79a3f652b7ca
Elayd <iamvine@mail.ru>
2022-04-20 19:45:57 +0300

    last vers

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 'use strict';
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = ["count", "movies", "actors","genres", "privat"];
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-personalMovieDB[0] = numberOfFilms;
-personalMovieDB[1] = {};
-personalMovieDB[2] ={};
-personalMovieDB[3] =[];
-personalMovieDB[4] = false;
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцените его?", "");
 
-console.log(personalMovieDB[0]);
-console.log(personalMovieDB[1]);
-console.log(personalMovieDB[2]);
-console.log(personalMovieDB[3]);
-console.log(personalMovieDB[4]);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
